cdb2161e0919c0ee8ff39c6a25e85e3570167ea2
Sushant Gupta <sushantg339@gmail.com>
2025-08-02 20:18:04 +0530

    day 14 learning update - authentication using JWT

--- day 12 and 13 - authentication/src/routes/auth.routes.js
+++ day 12 and 13 - authentication/src/routes/auth.routes.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const router = express.Router()
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
 
 
 router.post('/register' , async (req,res)=>{
     const {username , password} = req.body
+
     const user = await userModel.create({
         username ,
         password
     })
 
+
+    const token = jwt.sign({
+        id : user._id,
+
+    },process.env.JWT_SECRET)
+
+    res.cookie('token' , token)
+
+
     res.status(201).json({
         message : 'user registered successfully',
-        user
+        user,
+        token
     })
 })
 
@@ -41,6 +53,34 @@ router.post('/login' , async (req,res)=>{
     })
 })
 
+router.get('/user', async (req,res)=>{
+    const {token} = req.cookies
+    if(!token){
+        return res.status(401).json({
+            message : "unauthorised access. Invalid Token"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token , process.env.JWT_SECRET)
+    
+        const user = await userModel.findOne({
+            _id : decoded.id
+        }).select('-password -__v' )
+
+        res.status(200).json({
+            message : "user data fetched successfully",
+            user
+        })
+        
+    } catch (error) {
+        return res.status(401).json({
+            message : "unauthorised - invalid token"
+        })
+    }
+
+
+})
 
 
 module.exports = router
