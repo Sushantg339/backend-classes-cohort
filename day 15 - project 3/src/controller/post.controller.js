e4359397a158a92367eb5c235c245aa39b97775b
Sushant Gupta <sushantg339@gmail.com>
2025-08-05 21:24:41 +0530

    day 19 - implemented imagekit cloud upload of images and Project 3 Completed

--- day 15 - project 3/src/controller/post.controller.js
+++ day 15 - project 3/src/controller/post.controller.js
@@ -1,17 +1,37 @@
-const postModel = require('../models/post.model')
-const generateCaption = require('../services/ai.service')
-const createPostController = async (req,res) => {
-    const file = req.file
+const postModel = require('../models/post.model');
+const generateCaption = require('../services/ai.service');
+const uploadFile = require('../services/storage.service');
+const { v4: uuidv4 } = require('uuid');
 
-    const base64ImageFile = new Buffer.from(file.buffer).toString('base64')
+const createPostController = async (req, res) => {
+    try {
+        const file = req.file;
 
-    const caption = await generateCaption(base64ImageFile)
+        if (!file) {
+            return res.status(400).json({ message: "No file uploaded" });
+        }
 
-    res.json({
-        caption 
-    })
-}
+        const base64ImageFile = Buffer.from(file.buffer).toString('base64');
+        const caption = await generateCaption(base64ImageFile);
+
+        const result = await uploadFile(file.buffer, `${uuidv4()}`);
+
+        const post = await postModel.create({
+            caption,
+            image: result.url,
+            user: req.user.id
+        });
+
+        res.status(201).json({
+            message: "Post created successfully",
+            post
+        });
+    } catch (error) {
+        console.error("Error creating post:", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
 
 module.exports = {
     createPostController
-}
+};
